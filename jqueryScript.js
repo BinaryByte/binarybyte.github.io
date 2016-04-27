071243ef8fc308bb5d625341b54a63dd448a106a
Tyly04 <tyly04@gmail.com>
2016-04-27 11:12:11 -0700

    Update jqueryScript.js

--- jqueryScript.js
+++ jqueryScript.js
@@ -1,9 +1,5 @@
 $(document).ready(
   function(){
-    $("#question1").click(
-      function(){
-        $("#question2").fadeIn("slow");
-      }
-      );
+    $("#question2").fadeIn("slow");
   }
   );
