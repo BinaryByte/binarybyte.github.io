e1f5e8fe8be7cdc466e5c414fe7ce3fc42c33b7a
Tyly04 <tyly04@gmail.com>
2016-04-27 08:55:19 -0700

    Update jqueryScript.js

--- jqueryScript.js
+++ jqueryScript.js
@@ -1,12 +1,5 @@
 $(document).ready(
   function(){
-  $("div").hover(
-    function(){
-      $(this).css("background-color","black");
-    }, 
-    function(){
-      $(this).css("background-color","purple");
-    }
-    );
+    
   }
   );
